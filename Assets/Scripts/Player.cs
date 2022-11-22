2268d85f92ca2bcf64f693e5e0203d736b6b9330
ltsme <4065818+ltsme@users.noreply.github.com>
2022-11-22 20:50:43 +0900

    InputSystem 추가 및 Player.cs 파일 수정
    InputSystem 추가 및 Player.cs 파일 수정
    Player.cs 파일 위치 변경 (Assets/Scripts)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed = 5;
     Rigidbody2D rigid;
-
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +21,16 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // GetAxis -> GetAxisRaw 미끄러지지 않는 조작감
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+    // void Update()
+    // {
+    //     // GetAxis -> GetAxisRaw 미끄러지지 않는 조작감
+    //     inputVec.x = Input.GetAxisRaw("Horizontal");
+    //     inputVec.y = Input.GetAxisRaw("Vertical");
+    // }
+
+
+    void OnMove(InputValue value){
+        inputVec = value.Get<Vector2>();
     }
 
     // 물리 연산 프레임마다 호출되는 생명주기 함수
@@ -39,7 +44,7 @@ public class Player : MonoBehaviour
         // 3. 위치 이동
         // MovePosition은 위치 이동이라 현재 위치도 더해주어야 함.
         // FixedUpdate 에서는 fixedDeltaTime을 쓴다.
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // normalized : 벡터 값의 크기가 1이 되도록 수정 (대각선의 경우 루트2 이므로)
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime; // normalized : 벡터 값의 크기가 1이 되도록 수정 (대각선의 경우 루트2 이므로)
         rigid.MovePosition(rigid.position + nextVec);
     }
 }
